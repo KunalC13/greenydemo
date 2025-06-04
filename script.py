d9f7fc68e14c578abbc68ef2a5e8575fb8c47cba
kunal-chopra1 <kunal.chopra1@vodafone.com>
2025-06-04 11:47:46 +0200

    debugging

--- script.py
+++ script.py
@@ -3,11 +3,10 @@ from codecarbon import EmissionsTracker
 tracker = EmissionsTracker()
 tracker.start()
 
-# Sample CPU-heavy task (Fibonacci)
 def fib(n):
     return n if n <= 1 else fib(n - 1) + fib(n - 2)
 
-fib(28)
+fib(30)
 
 emissions = tracker.stop()
 print(f"Emissions: {emissions} kg CO₂")
